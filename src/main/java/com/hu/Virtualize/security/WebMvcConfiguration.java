fb28307eba38c8c54da364d62e3cafaf4d587b33
Saifi, Zhatab <zsaifi@deloitte.com>
2021-05-22 00:13:15 +0530

    zhatab: now swagger is run after adding CorsRegistry

--- src/main/java/com/hu/Virtualize/security/WebMvcConfiguration.java
+++ src/main/java/com/hu/Virtualize/security/WebMvcConfiguration.java
@@ -2,6 +2,7 @@ package com.hu.Virtualize.security;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
@@ -9,8 +10,27 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 @EnableWebMvc
 public class WebMvcConfiguration extends WebMvcConfigurerAdapter {
 
+    /**
+     * This function will give access to only localhost:4200 application to access the apis
+     * @param registry registry
+     */
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("http://localhost:4200");
+        registry.addMapping("/**") .allowedOrigins("http://localhost:4200");
+    }
+
+    /**
+     * This function will add the swagger in application
+     * @param registry registry
+     */
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+
     }
 }
