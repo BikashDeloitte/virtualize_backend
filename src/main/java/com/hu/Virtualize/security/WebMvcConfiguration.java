2f7a29acc3a846f80d91c500ba2bcf18f7cd4478
Tripathi, Praveen Kumar <pravtripathi@deloitte.com>
2021-05-26 16:28:38 +0530

    added security dependency

--- src/main/java/com/hu/Virtualize/security/WebMvcConfiguration.java
+++ src/main/java/com/hu/Virtualize/security/WebMvcConfiguration.java
@@ -1,12 +1,9 @@
 package com.hu.Virtualize.security;
+
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
@@ -19,7 +16,7 @@ public class WebMvcConfiguration extends WebMvcConfigurerAdapter {
      */
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("http://localhost:4200");
+        registry.addMapping("/**") .allowedOrigins("http://localhost:4200");
     }
 
     /**
@@ -34,15 +31,5 @@ public class WebMvcConfiguration extends WebMvcConfigurerAdapter {
 
         registry.addResourceHandler("/webjars/**")
                 .addResourceLocations("classpath:/META-INF/resources/webjars/");
-    }
-}
-
-@Configuration
-@EnableWebSecurity
-class SecurityConfig extends WebSecurityConfigurerAdapter {
-
-    @Override
-    protected void configure(HttpSecurity security) throws Exception {
-        security.httpBasic().disable();
     }
 }
