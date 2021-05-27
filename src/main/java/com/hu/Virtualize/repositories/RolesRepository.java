e6a7727f19f3f996a5e214441b40f2d884088b2b
Tripathi, Praveen Kumar <pravtripathi@deloitte.com>
2021-05-27 15:57:38 +0530

    added security class

--- src/main/java/com/hu/Virtualize/repositories/RolesRepository.java
+++
@@ -1,7 +0,0 @@
-package com.hu.Virtualize.repositories;
-
-import com.hu.Virtualize.entities.Roles;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RolesRepository extends JpaRepository<Roles,Long> {
-}
